9b2dae39b1558aefe4949e27c2b279fa48724871
GameCrahser0 <andrewolski0@gmail.com>
2022-08-02 19:56:42 -0300

    primeiro commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        funcaoSoma(2,2);
+
+    }
+
+    public static void funcaoSoma(int x, int y){
+        int z = x+y;
+        System.out.println("A soma de " + x + "+" + y + " é igual a :"+z);
+    }
+}
