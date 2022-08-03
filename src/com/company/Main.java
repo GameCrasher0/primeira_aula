24064860ff49ea7f57089f641f979f446548fb81
GameCrahser0 <andrewolski0@gmail.com>
2022-08-02 22:07:55 -0300

    mais codigo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,30 +2,41 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) {
-        funcaoSoma(2,2);
-        funcaoSubtrair(5,2);
-        List<String> x = nomesDeAlunos("Lucas", "Fernando");
-        for (String s: x){
-            System.out.println(s);
-        }
-    }
-    public static List<String> nomesDeAlunos(String s, String str){
-        List<String> listSring = new ArrayList<>();
-        listSring.add(s);
-        listSring.add(str);
-        return listSring;
-        }
-    }
-    public static void funcaoSubtrair(double x, double y){
-        double z = x - y;
+// funcaoSoma(2,2);
+// funcaoSubtrair(5,2);
+// List<String> x = nomesDeAlunos("Paulo", "Felipe");
+// for (String s: x){
+// System.out.println(s);
+// }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual o seu nome?");
+        String s = scanner.nextLine();
+        System.out.println("Que ano você nasceu?");
+        int ano = Integer.parseInt(scanner.nextLine());
+        int idade = 2022-ano;
+        System.out.println("Nome:" + s);
+        System.out.println("Idade:" + idade);
 
     }
-    public static void funcaoSoma(int x, int y){
+    public static List<String> nomesDeAlunos(String s, String str){
+        List<String> ListString = new ArrayList<>();
+        ListString.add(s);
+        ListString.add(str);
+        return ListString;
+
+
+    }
+    public static void funcaoSubtrair (double x, double y){
+        double z = x - y;
+    }
+    public static void funcaoSoma(int x,int y){
         int z = x+y;
-        System.out.println("A soma de " + x + "+" + y + " é igual a :"+z);
+        System.out.println("a soma de "+ x + " mais "+ y+ " é igual a " + z);
     }
 }
