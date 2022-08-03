b38a984acceeb362191af0ff4daaf44f98c47627
GameCrahser0 <andrewolski0@gmail.com>
2022-08-02 21:04:24 -0300

    Segunda Função

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         funcaoSoma(2,2);
+        funcaoSubtrair(5,2);
+    }
+    public static void funcaoSubtrair(double x, double y){
+        double z = x - y;
 
     }
-
     public static void funcaoSoma(int x, int y){
         int z = x+y;
         System.out.println("A soma de " + x + "+" + y + " é igual a :"+z);
